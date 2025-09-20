a937ba9208881f5cf6b4c156d594bb73829f8c8f
tomy08 <tomasesantacruz@gmail.com>
2025-09-20 17:42:20 -0300

    Add PostgreSQL support with type definitions and database connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { Pool } from 'pg'
 
 const app = express()
 dotenv.config()
 const PORT = process.env.PORT || 4000
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+pool
+  .connect()
+  .then(() => console.log('Conexión a la base de datos establecida'))
+  .catch((err: Error) =>
+    console.error('Error al conectar a la base de datos:', err)
+  )
 
 app.use(cors())
 app.use(express.json())
